8620b28cbd6e6a1410105e3bccdb632bf2dad0b7
Sdanisch <sdanisch@gmail.com>
2015-09-24 17:17:38 +0200

    get rid of TestImages
    TestImages seems to fail on julia 0.4

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,75 +1,11 @@
-using OSXNativeIO, FactCheck, Images, Colors, FixedPointNumbers, TestImages
+using OSXNativeIO, FactCheck, Images
 
 facts("OS X reader") do
-    context("Autumn leaves") do
-        img = testimage("autumn_leaves")
-        @fact colorspace(img) --> "RGBA"
-        @fact ndims(img) --> 2
-        @fact colordim(img) --> 0
-        @fact eltype(img) --> RGBA{Ufixed16}
-    end
-    context("Camerman") do
-        img = testimage("cameraman")
-        @fact colorspace(img) --> "Gray"
-        @fact ndims(img) --> 2
-        @fact colordim(img) --> 0
-        @fact eltype(img) --> Gray{Ufixed8}
-    end
-    context("Earth Apollo") do
-        img = testimage("earth_apollo17")
-        @fact colorspace(img) --> "RGB4"
-        @fact ndims(img) --> 2
-        @fact colordim(img) --> 0
-        @fact eltype(img) --> RGB4{Ufixed8}
-    end
-    context("Fabio") do
-    img = testimage("fabio")
-        @fact colorspace(img) --> "Gray"
-        @fact ndims(img) --> 2
-        @fact colordim(img) --> 0
-        @fact eltype(img) --> Gray{Ufixed8}
-    end
-    context("House") do
-        img = testimage("house")
-        @fact colorspace(img) --> "GrayA"
-        @fact ndims(img) --> 2
-        @fact colordim(img) --> 0
-        @fact eltype(img) --> GrayA{Ufixed8}
-    end
-    context("Jetplane") do
-        img = testimage("jetplane")
-        @fact colorspace(img) --> "GrayA"
-        @fact ndims(img) --> 2
-        @fact colordim(img) --> 0
-        @fact eltype(img) --> GrayA{Ufixed8}
-    end
-    context("Lighthouse") do
-        img = testimage("lighthouse")
-        @fact colorspace(img) --> "RGB4"
-        @fact ndims(img) --> 2
-        @fact colordim(img) --> 0
-        @fact eltype(img) --> RGB4{Ufixed8}
-    end
-    context("Mandrill") do
-        img = testimage("mandrill")
-        @fact colorspace(img) --> "RGB"
-        @fact ndims(img) --> 2
-        @fact colordim(img) --> 0
-        @fact eltype(img) --> RGB{Ufixed8}
-    end
-    context("Moonsurface") do
-        img = testimage("moonsurface")
-        @fact colorspace(img) --> "Gray"
-        @fact ndims(img) --> 2
-        @fact colordim(img) --> 0
-        @fact eltype(img) --> Gray{Ufixed8}
-    end
-    context("Mountainstream") do
-        img = testimage("mountainstream")
-        @fact colorspace(img) --> "RGB4"
-        @fact ndims(img) --> 2
-        @fact colordim(img) --> 0
-        @fact eltype(img) --> RGB4{Ufixed8}
+	imagedir = Pkg.dir("OSXNativeIO", "test", "images")
+    images = filter(x-> splitext(x)[2] != ".psd", readdir(imagedir))
+    for image in images
+    	img = imread(joinpath(imagedir, image))
+    	@fact isa(img, Image) --> true
     end
 end
 
