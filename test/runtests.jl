d23fb5b2eebc11c72b748aa2faddf0005bea316a
Sdanisch <sdanisch@gmail.com>
2015-09-24 17:30:09 +0200

    removed unsupported image extensions

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using OSXNativeIO, FactCheck, Images
 
 facts("OS X reader") do
 	imagedir = Pkg.dir("OSXNativeIO", "test", "images")
-    images = filter(x-> splitext(x)[2] != ".psd", readdir(imagedir))
+    images = readdir(imagedir)
     for image in images
     	context(image) do
 	    	img = imread(joinpath(imagedir, image))
